29e606352ea4a8ec1f0a127eedee36c82ba89be5
Esrae Ben Selma <isra.ben.selma@gmail.com>
2025-10-22 16:39:57 +0200

    Create database and retail_sales table schema

--- SQL/00_schema.sql
+++ SQL/00_schema.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE IF NOT EXISTS sql_project_p1;
+USE sql_project_p1;
 
+DROP TABLE IF EXISTS retail_sales;
+
+CREATE TABLE retail_sales (
+    transactions_id   INT PRIMARY KEY,
+    sale_date         DATE,
+    sale_time         TIME,
+    customer_id       INT,
+    gender            VARCHAR(25),
+    age               INT,
+    category          VARCHAR(25),
+    quantity          INT,
+    price_per_unit    FLOAT,
+    cogs              FLOAT,
+    total_sale        FLOAT
+);
